f19922f34e8c16bc80ed0e58dae356fa19fa278f
John Schoeman <johnschoeman1617@gmail.com>
2020-03-22 11:14:42 -0400

    decode data and start rendering it

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
 import Http
 import Json.Decode as Decode
+import LineChart exposing (view1)
 
 
 
@@ -12,7 +13,8 @@ import Json.Decode as Decode
 
 
 type alias Model =
-    { data : Maybe CoronaData }
+    { data : Maybe CoronaData
+    }
 
 
 init : ( Model, Cmd Msg )
@@ -21,11 +23,13 @@ init =
 
 
 type alias CoronaData =
-    { us : List DayData
+    { us : List CoronaDatum
+    , italy : List CoronaDatum
+    , china : List CoronaDatum
     }
 
 
-type alias DayData =
+type alias CoronaDatum =
     { date : String
     , confirmed : Int
     , deaths : Int
@@ -63,13 +67,15 @@ getCoronaData =
 
 coronaDataDecoder : Decode.Decoder CoronaData
 coronaDataDecoder =
-    Decode.map CoronaData
+    Decode.map3 CoronaData
         (Decode.field "US" (Decode.list dayDataDecoder))
+        (Decode.field "Italy" (Decode.list dayDataDecoder))
+        (Decode.field "China" (Decode.list dayDataDecoder))
 
 
-dayDataDecoder : Decode.Decoder DayData
+dayDataDecoder : Decode.Decoder CoronaDatum
 dayDataDecoder =
-    Decode.map4 DayData
+    Decode.map4 CoronaDatum
         (Decode.field "date" Decode.string)
         (Decode.field "confirmed" Decode.int)
         (Decode.field "deaths" Decode.int)
@@ -82,10 +88,38 @@ dayDataDecoder =
 
 view : Model -> Html Msg
 view model =
+    let
+        maybeData =
+            model.data
+    in
+    case maybeData of
+        Nothing ->
+            div [] [ text "loading" ]
+
+        Just data ->
+            div [] [ countryChart data.us ]
+
+
+
+-- countyChart
+
+
+datumElement : CoronaDatum -> Html msg
+datumElement datum =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ text datum.date
+        , text <| String.fromInt datum.confirmed
+        , text <| String.fromInt datum.deaths
+        , text <| String.fromInt datum.recovered
         ]
+
+
+dataForCountry : List CoronaDatum -> Html msg
+dataForCountry listData =
+    div [] <|
+        List.map
+            datumElement
+            listData
 
 
 
